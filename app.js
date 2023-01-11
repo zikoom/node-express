f92b74d0b0012cb8e6f55c8e4f696fd343207932
koom <hunmonhun@gmail.com>
2023-01-11 15:09:08 +0900

    cors 옵션 추가

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "*"
+    origin: "*",
+    credentials :true
   }
 });
 
