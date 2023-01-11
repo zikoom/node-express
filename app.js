6404c9d30b56f18532f01adc4472b7213fb74607
koom <hunmonhun@gmail.com>
2023-01-11 16:05:04 +0900

    app use cors

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //소켓 서버
 const httpServer = require('http').createServer(app);
 
