252f4563c7c11dae9bc674388916e6b9f81d22e5
koom <hunmonhun@gmail.com>
2023-01-12 17:55:10 +0900

    cors 수정

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const cors = require('cors');
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 io.on('connection', (socket) =>{
   console.log('client connected');
